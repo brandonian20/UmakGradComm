e6a4ef4d58b660f6c7f79193ca60a0098077f85e
eliasezar27 <eliasezar27@gmail.com>
2022-07-07 11:36:19 +0800

    Banner background gradient darken. Graduands -> Graduates

--- resources/views/visitor/home.blade.php
+++ resources/views/visitor/home.blade.php
@@ -10,7 +10,7 @@
                         <h1 class="mb-30 mt-70 px-lg-30 text-white" data-show="startbox">CONGRATULATIONS<br><span class="highlight"> BATCH 2022</span></h1>
                         <a data-show="startbox" class="btn btn-accent-1 align-items-center" data-bs-toggle="modal" data-bs-target="#viewCollege" target="_self">
                             <svg class="svg-inline me-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-graduate" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M45.63 79.75L52 81.25v58.5C45 143.9 40 151.3 40 160c0 8.375 4.625 15.38 11.12 19.75L35.5 242C33.75 248.9 37.63 256 43.13 256h41.75c5.5 0 9.375-7.125 7.625-13.1L76.88 179.8C83.38 175.4 88 168.4 88 160c0-8.75-5-16.12-12-20.25V87.13L128 99.63l.001 60.37c0 70.75 57.25 128 128 128s127.1-57.25 127.1-128L384 99.62l82.25-19.87c18.25-4.375 18.25-27 0-31.5l-190.4-46c-13-3-26.62-3-39.63 0l-190.6 46C27.5 52.63 27.5 75.38 45.63 79.75zM359.2 312.8l-103.2 103.2l-103.2-103.2c-69.93 22.3-120.8 87.2-120.8 164.5C32 496.5 47.53 512 66.67 512h378.7C464.5 512 480 496.5 480 477.3C480 400 429.1 335.1 359.2 312.8z"></path></svg>
-                            View Graduands</a>
+                            View Graduates</a>
                     </div>
                     <div class="text-center">
                         <span class="bg-warning bg-opacity-75">
@@ -27,7 +27,7 @@
                         <div class="background">
                             <div class="background-image jarallax" data-jarallax data-speed="0.8"><img class="jarallax-img" loading="lazy" src="{{ asset('/img/umak_banner.jpg') }}" data-swiper-parallax-x="20%" alt=""></div>
                             <div class="background-color" style="--background-color: #000; opacity: .25;"></div>
-                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0) 150px);"></div>
+                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.6) 100%, rgba(0, 0, 0, 0) 150px);"></div>
                         </div>
                         
                     </div>
@@ -37,7 +37,7 @@
                         <div class="background">
                             <div class="background-image jarallax" data-jarallax data-speed="0.8"><img class="jarallax-img" loading="lazy" src="{{ asset('/img/umak_banner2.png') }}" data-swiper-parallax-x="20%" alt=""></div>
                             <div class="background-color" style="--background-color: #000; opacity: .25;"></div>
-                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0) 150px);"></div>
+                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.6) 100%, rgba(0, 0, 0, 0) 150px);"></div>
                         </div>
                         
                     </div>
@@ -47,7 +47,7 @@
                         <div class="background">
                             <div class="background-image jarallax" data-jarallax data-speed="0.8"><img class="jarallax-img" loading="lazy" src="{{ asset('/img/umak_banner3.png') }}" data-swiper-parallax-x="20%" alt=""></div>
                             <div class="background-color" style="--background-color: #000; opacity: .25;"></div>
-                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0) 150px);"></div>
+                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.6) 100%, rgba(0, 0, 0, 0) 150px);"></div>
                         </div>
                         
                     </div>
@@ -57,7 +57,7 @@
                         <div class="background">
                             <div class="background-image jarallax" data-jarallax data-speed="0.8"><img class="jarallax-img" loading="lazy" src="{{ asset('/img/umak_banner4.png') }}" data-swiper-parallax-x="20%" alt=""></div>
                             <div class="background-color" style="--background-color: #000; opacity: .25;"></div>
-                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0) 150px);"></div>
+                            <div class="background-color" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.6) 100%, rgba(0, 0, 0, 0) 150px);"></div>
                         </div>
                     </div>
                 </div>
