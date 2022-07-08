c352c5b8fbc54729f78a0e9c5be6f36394439e7e
eliasezar27 <eliasezar27@gmail.com>
2022-07-08 13:17:44 +0800

    Fixed view graduates modal

--- resources/views/visitor/home.blade.php
+++ resources/views/visitor/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="d-flex flex-wrap justify-content-center align-items-center h-100">
                     <div class="text-center">
                         <h1 class="mb-30 mt-70 px-lg-30 text-white" data-show="startbox">CONGRATULATIONS<br><span class="highlight"> BATCH 2022</span></h1>
-                        <a data-show="startbox" class="btn btn-accent-1 align-items-center" data-bs-toggle="modal" data-bs-target="#viewCollege" target="_self">
+                        <a data-show="startbox" class="btn btn-accent-4 align-items-center" data-bs-toggle="modal" data-bs-target="#viewCollege" target="_self">
                             <svg class="svg-inline me-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-graduate" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M45.63 79.75L52 81.25v58.5C45 143.9 40 151.3 40 160c0 8.375 4.625 15.38 11.12 19.75L35.5 242C33.75 248.9 37.63 256 43.13 256h41.75c5.5 0 9.375-7.125 7.625-13.1L76.88 179.8C83.38 175.4 88 168.4 88 160c0-8.75-5-16.12-12-20.25V87.13L128 99.63l.001 60.37c0 70.75 57.25 128 128 128s127.1-57.25 127.1-128L384 99.62l82.25-19.87c18.25-4.375 18.25-27 0-31.5l-190.4-46c-13-3-26.62-3-39.63 0l-190.6 46C27.5 52.63 27.5 75.38 45.63 79.75zM359.2 312.8l-103.2 103.2l-103.2-103.2c-69.93 22.3-120.8 87.2-120.8 164.5C32 496.5 47.53 512 66.67 512h378.7C464.5 512 480 496.5 480 477.3C480 400 429.1 335.1 359.2 312.8z"></path></svg>
                             View Graduates</a>
                     </div>
@@ -281,45 +281,45 @@
                 <div class="">
                     <!-- First Row -->
                     <div class="row py-10">
-                      <div class="col">
+                      <div class="col d-grid gap-2">
 
-                        <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                        <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                             <div class="row align-items-center">
                                 <div class="col-4">
-                                    <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                    <img src="{{ asset('/img/coahs-logo.png') }}"  width="70px"> 
                                 </div>
-                                <div class="col-8 text-center">
-                                    College of Allied Health Studies
+                                <div class="col-8 text-center fs-3">
+                                    COAHS
 
                                 </div>
                             </div>
                         </a>
 
                       </div>
-                      <div class="col">
+                      <div class="col d-grid gap-2">
                         
-                        <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                        <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                             <div class="row align-items-center">
                                 <div class="col-4">
-                                    <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                    <img src="{{ asset('/img/cal-logo-w.png') }}"  width="70px"> 
                                 </div>
-                                <div class="col-8 text-center">
-                                    College of Arts and Letters
+                                <div class="col-8 text-center fs-3">
+                                    CAL
 
                                 </div>
                             </div>
                         </a>
 
                       </div>
-                      <div class="col">
+                      <div class="col d-grid gap-2">
                         
-                        <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                        <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                             <div class="row align-items-center">
                                 <div class="col-4">
                                     <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
                                 </div>
-                                <div class="col-8 text-center">
-                                    College of Business and Financial Sciences
+                                <div class="col-8 text-center fs-3">
+                                    CBFS
 
                                 </div>
                             </div>
@@ -329,43 +329,43 @@
                     </div>
                     <!-- Second Row -->
                     <div class="row py-10">
-                        <div class="col">
-                          <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                        <div class="col d-grid gap-2">
+                          <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                             <div class="row align-items-center">
                                 <div class="col-4">
-                                    <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                    <img src="{{ asset('/img/ccis-logo.png') }}"  width="70px"> 
                                 </div>
-                                <div class="col-8 text-center">
-                                    College of Computing and Information Sciences
+                                <div class="col-8 text-center fs-3">
+                                    CCIS
 
                                 </div>
                             </div>
                         </a>
                         </div>
-                        <div class="col">
+                        <div class="col d-grid gap-2">
                           
-                            <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                            <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                 <div class="row align-items-center">
                                     <div class="col-4">
-                                        <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                        <img src="{{ asset('/img/ccse updated.png') }}"  width="70px"> 
                                     </div>
-                                    <div class="col-8 text-center">
-                                        College of Construction Sciences and Engineering
+                                    <div class="col-8 text-center fs-3">
+                                        CCSE
 
                                     </div>
                                 </div>
                             </a>
 
                         </div>
-                        <div class="col">
+                        <div class="col d-grid gap-2">
                           
-                            <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                            <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                 <div class="row align-items-center">
                                     <div class="col-4">
-                                        <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                        <img src="{{ asset('/img/ccaps-logo-w.png') }}"  width="70px"> 
                                     </div>
-                                    <div class="col-8 text-center">
-                                        College of Continuing, Advanced and Professional Studies
+                                    <div class="col-8 text-center fs-3">
+                                        CCAPS
 
                                     </div>
                                 </div>
@@ -375,45 +375,45 @@
                       </div>
                       <!-- Third Row -->
                         <div class="row py-10">
-                            <div class="col">
+                            <div class="col d-grid gap-2">
                             
-                                <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                                <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                     <div class="row align-items-center">
                                         <div class="col-4">
-                                            <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                            <img src="{{ asset('/img/coe-logo.png') }}"  width="70px"> 
                                         </div>
-                                        <div class="col-8 text-center">
-                                            College of Education
+                                        <div class="col-8 text-center fs-3">
+                                            COE
     
                                         </div>
                                     </div>
                                 </a>
 
                             </div>
-                            <div class="col">
+                            <div class="col d-grid gap-2">
                             
-                                <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                                <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                     <div class="row align-items-center">
                                         <div class="col-4">
-                                            <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                            <img src="{{ asset('/img/cgpp-logo.png') }}"  width="70px"> 
                                         </div>
-                                        <div class="col-8 text-center">
-                                            College of Governance and Public Policy
+                                        <div class="col-8 text-center fs-3">
+                                            CGPP
     
                                         </div>
                                     </div>
                                 </a>
 
                             </div>
-                            <div class="col">
+                            <div class="col d-grid gap-2">
                             
-                                <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                                <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                     <div class="row align-items-center">
                                         <div class="col-4">
-                                            <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                            <img src="{{ asset('/img/chk-logo.png') }}"  width="70px"> 
                                         </div>
-                                        <div class="col-8 text-center">
-                                            College of Human Kinetics
+                                        <div class="col-8 text-center fs-3">
+                                            CHK
     
                                         </div>
                                     </div>
@@ -423,45 +423,45 @@
                         </div>
                       <!-- Fourth Row -->
                         <div class="row py-10">
-                            <div class="col">
+                            <div class="col d-grid gap-2">
                             
-                                <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                                <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                     <div class="row align-items-center">
                                         <div class="col-4">
-                                            <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                            <img src="{{ asset('/img/cos-logo.png') }}"  width="70px"> 
                                         </div>
-                                        <div class="col-8 text-center">
-                                            College of Science
+                                        <div class="col-8 text-center fs-3">
+                                            COS
     
                                         </div>
                                     </div>
                                 </a>
 
                             </div>
-                            <div class="col">
+                            <div class="col d-grid gap-2">
                             
-                                <a type="button" class="btn btn-accent-1 rounded-3 shadow" href="#" target="_self">
+                                <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                     <div class="row align-items-center">
                                         <div class="col-4">
-                                            <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                            <img src="{{ asset('/img/ctm-logo.png') }}"  width="70px"> 
                                         </div>
-                                        <div class="col-8 text-center">
-                                            College of Technology Management
+                                        <div class="col-8 text-center fs-3">
+                                            CTM
     
                                         </div>
                                     </div>
                                 </a>
 
                             </div>
-                            <div class="col">
+                            <div class="col d-grid gap-2">
                             
-                                <a type="button" class="btn btn-accent-1 rounded-3 shadow btn-block" href="#" target="_self">
+                                <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                     <div class="row align-items-center">
                                         <div class="col-4">
-                                            <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                            <img src="{{ asset('/img/cthm-logo.png') }}"  width="70px"> 
                                         </div>
-                                        <div class="col-8 text-center">
-                                            College of Tourism and Hospitality Management
+                                        <div class="col-8 text-center fs-3">
+                                            CTHM
     
                                         </div>
                                     </div>
@@ -474,14 +474,14 @@
                             <div class="col">
                             </div>
 
-                            <div class="col">
-                                <a type="button" class="btn btn-accent-1 rounded-3 shadow btn-block" href="#" target="_self">
+                            <div class="col d-grid gap-2">
+                                <a type="button" class="btn btn-accent-4 rounded-3 shadow" href="#" target="_self">
                                     <div class="row align-items-center">
                                         <div class="col-4">
-                                            <img src="{{ asset('/img/cbfs-logo.png') }}"  width="70px"> 
+                                            <img src="{{ asset('/img/sol-logo.png') }}"  width="70px"> 
                                         </div>
-                                        <div class="col-8 text-center">
-                                            School of Law
+                                        <div class="col-8 text-center fs-3">
+                                            SOL
     
                                         </div>
                                     </div>
