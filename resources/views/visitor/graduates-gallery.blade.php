6b985994a4f6aaeb20883212f0a81410c6dd99b6
Tim Cuizon <timothycuizon@outlook.com>
2022-07-21 11:40:44 +0800

    Graduates Gallery and Messages

--- resources/views/visitor/graduates-gallery.blade.php
+++ resources/views/visitor/graduates-gallery.blade.php
@@ -1,22 +1,98 @@
 @extends('components.layout')
 
 @section('content')
-     <!-- Programme Content -->
-     <section id="programme">
-        <div class="pt-120 pb-130 alert-warning shape-parent text-center">
-            <!-- Shape-->
-            <div class="shape justify-content-end"><img loading="lazy" src="{{ asset('img/root/404-shape-326x321.png') }}"
-                    alt="" width="542" height="382"></div><!-- Shape-->
-            <div class="shape align-items-end"><img loading="lazy"
-                    src="{{ asset('/img/root/contact-2-shape-558x364.png') }}" alt="" width="309"
-                    height="435"></div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 offset-lg-3" style="margin-bottom: 5rem">
-                        <h2 class="m-0" data-show="startbox">Graduates</h2>
-                    </div>
-                </div>
+<section class="graduates-bg pt-100 pb-100">
+    <section id="questions" class="pt-4">
+        <div class="container">
+            <div class="d-flex justify-content-center">
+                <img src="http://127.0.0.1:8000/img/ccis-logo.png" class="img-fluid  me-3 " style="width: 100px;">
+            </div>
+            <div class="">
+                <h1 class="text-center mb-4 text-light mt-3" style="font-family: Marcellus;">College of Computing and Information Sciences</h2>
             </div>
         </div>
     </section>
+
+  <!-- PROGRAM -->
+   <section class=" d-flex justify-content-center mb-3  " style="width:100%; ">
+    <div class="program_section " >
+      <div class="">
+        <div class="card text-center bg-dark rounded-5" >
+          <div class="card-header row border-0">
+            <div class=" d-flex pt-4 justify-content-center" >
+              <h3 class="text-light  " style="font-family: Marcellus;">Bachelor of Science in Computer Science Major in Application Development</h2>
+            </div>
+          </div>
+          <div class="card-body mt-0" >
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="{{ asset('/img/sample.jpg') }}" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+
+            <!-- <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+            <a data-bs-toggle="modal" data-bs-target="#slide-deck"><img src="elias.jpg" class="card-img-top mx-2 mb-3 rounded grad_pic"  alt="..." ></a>
+-->
+
+          </div>
+          <!-- <div class=" card-footer text-muted border-0">
+            <h6>@Batch 2022<h6>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Modal -->
+  <div class="modal fade" id="slide-deck" tabindex="-1" aria-labelledby="slide-deckLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl" >
+      <div class="modal-content">
+        <div class="modal-body" style="background-color: #d5ac44; padding: 0.5rem !important">
+          <img src="{{ asset('/img/slide-deck.png') }}" class="card-img-top  rounded img-fluid " alt="..."  style="max-width: 100%">
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
--- resources/views/visitor/message.blade.php
+++ resources/views/visitor/message.blade.php
@@ -1,22 +1,103 @@
 @extends('components.layout')
 
 @section('content')
-     <!-- Programme Content -->
-     <section id="programme">
-        <div class="pt-120 pb-130 alert-warning shape-parent text-center">
-            <!-- Shape-->
-            <div class="shape justify-content-end"><img loading="lazy" src="{{ asset('img/root/404-shape-326x321.png') }}"
-                    alt="" width="542" height="382"></div><!-- Shape-->
-            <div class="shape align-items-end"><img loading="lazy"
-                    src="{{ asset('/img/root/contact-2-shape-558x364.png') }}" alt="" width="309"
-                    height="435"></div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 offset-lg-3" style="margin-bottom: 5rem">
-                        <h2 class="m-0" data-show="startbox">Message</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
+     <!-- Showcase -->
+    <section class="pt-100 pb-100 graduates-bg">
+        <section class=" p-5 text-center text-sm-start">
+           <div class="container">
+             <div class="d-sm-flex align-items-center justify-content-between">
+               <img class="img-fluid w-25 d-none d-sm-block" src="{{ asset('/img/binay.jpg') }}" alt="Showcase Photo" style="border-radius: 50%">
+               <div class="ms-5">
+                 <!-- <h1>Become a <span class="text-warning">Web Developer</span></h1> -->
+                 <h1 style="font-family: Marcellus; color: #d5ac44;">Message from the Mayor</h1>
+                 <p class="lead my-4"> <h4 style="font-family: Marcellus; color:white">
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role. </h4>
+                 </p>
+
+                 <!-- <button class="btn btn-primary btn-lg"
+                  data-bs-toggle="modal"
+                  data-bs-target="#enroll">Start The Enrollment</button> -->
+               </div>
+
+             </div>
+           </div>
+         </section>
+      
+         <section class=" p-2 text-center ">
+           <div class="container">
+             <div class="d-sm-flex align-items-center justify-content-between">
+               <div class="row">
+                 <div class="col-2">
+
+                 </div>
+              
+                 <div class="col-10" style="text-align: justify;">
+                 <!-- <h1>Become a <span class="text-warning">Web Developer</span></h1> -->
+                 <p class="lead my-4" style="font-family: Metropolis; color:white"> 
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                    <br><br>
+                
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+
+                   <br><br>
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                   We focus on teaching our students the fundamentals of the latest
+                   and greatest technologies to prepare them for their first dev role.
+                 </p>
+              
+                 <h5 style="font-family: Marcellus; font-weight: bold;" >Hon. Abigail Binay</h5>
+                 <h6 style="font-family: Marcellus;">City Mayor</h6>
+                 </div>
+              
+               </div>
+             </div>
+           </div>
+        </section>
     </section>
 @endsection
