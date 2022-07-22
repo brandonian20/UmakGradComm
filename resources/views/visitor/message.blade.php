3cd7e7ca17ea5165dac59c1cdf81ef7e912e8d0f
Tim Cuizon <timothycuizon@outlook.com>
2022-07-22 15:43:46 +0800

    New Message page

--- resources/views/visitor/message.blade.php
+++ resources/views/visitor/message.blade.php
@@ -2,40 +2,38 @@
 
 @section('content')
      <!-- Showcase -->
-    <section class="pt-100 pb-100 graduates-bg">
-        <section class=" p-5 text-center text-sm-start">
+      <div class="shape align-items-start justify-content-start" style="margin-left: -8rem;">
+        <img loading="lazy" src="{{ asset('/img/speakers/asset-2.svg') }}" alt="" width="420px" height="487">
+      </div>
+      <div class="shape align-items-end justify-content-end" style="margin-right: -8rem;">
+        <img loading="lazy" src="{{ asset('/img/speakers/asset-1.svg') }}" alt="" width="420px" height="487">
+      </div>
+    <section class="pt-lg-100 pb-100 mt-lg-100 pt-50 mt-50">
+        <section class="p-5 text-center text-sm-start mb-50">
            <div class="container">
-             <div class="d-sm-flex align-items-center justify-content-between">
-               <img class="img-fluid w-25 d-none d-sm-block" src="{{ asset('/img/binay.jpg') }}" alt="Showcase Photo" style="border-radius: 50%">
-               <div class="ms-5">
-                 <!-- <h1>Become a <span class="text-warning">Web Developer</span></h1> -->
-                 <h1 style="font-family: Marcellus; color: #d5ac44;">Message from the Mayor</h1>
-                 <p class="lead my-4"> <h4 style="font-family: Marcellus; color:white">
-                   We focus on teaching our students the fundamentals of the latest
-                   and greatest technologies to prepare them for their first dev role. </h4>
-                 </p>
-
-                 <!-- <button class="btn btn-primary btn-lg"
-                  data-bs-toggle="modal"
-                  data-bs-target="#enroll">Start The Enrollment</button> -->
-               </div>
-
+             <div class="row"> 
+                <div class="col-lg-8 col-12 text-lg-start text-center align-text-between">
+                   <!-- <h1>Become a <span class="text-warning">Web Developer</span></h1> -->
+                   <h1 style="font-family: Marcellus; color: #d5ac44;">Message from the OIC President and Concurrent Vice President for Academic Affairs</h1>
+                   <!-- <button class="btn btn-primary btn-lg"
+                    data-bs-toggle="modal"
+                    data-bs-target="#enroll">Start The Enrollment</button> -->
+                </div>
+                <div class="col-lg-4 col-12 d-flex justify-content-center"> 
+                   <img class="w-75 d-sm-block d-flex" src="{{ asset('/img/binay.jpg') }}" alt="Showcase Photo" style="border-radius: 50%">
+                </div>
              </div>
            </div>
          </section>
       
-         <section class=" p-2 text-center ">
+         <section class="p-2 text-center ">
            <div class="container">
              <div class="d-sm-flex align-items-center justify-content-between">
                <div class="row">
-                 <div class="col-2">
-
-                 </div>
-              
-                 <div class="col-10" style="text-align: justify;">
+                 <div class="col-12" style="text-align: justify;">
                  <!-- <h1>Become a <span class="text-warning">Web Developer</span></h1> -->
-                 <p class="lead my-4" style="font-family: Metropolis; color:white"> 
-                   We focus on teaching our students the fundamentals of the latest
+                 <p class="lead my-4 text-justify" style="font-family: Metropolis; "> 
+                   We focus on teaching our students the fundamentals of the latest,<br>
                    and greatest technologies to prepare them for their first dev role.
                    We focus on teaching our students the fundamentals of the latest
                    and greatest technologies to prepare them for their first dev role.
@@ -90,9 +88,8 @@
                    We focus on teaching our students the fundamentals of the latest
                    and greatest technologies to prepare them for their first dev role.
                  </p>
-              
-                 <h5 style="font-family: Marcellus; font-weight: bold;" >Hon. Abigail Binay</h5>
-                 <h6 style="font-family: Marcellus;">City Mayor</h6>
+
+                 <h3 style="font-family: Marcellus;"><br>Professor Ederson DT. Tapia, PhD. DPA</h3>
                  </div>
               
                </div>
