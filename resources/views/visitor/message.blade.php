fe998c8a52265633721289f76d720ebb566b1c4d
Tim Cuizon <timothycuizon@outlook.com>
2022-07-22 16:02:46 +0800

    Updated Message

--- resources/views/visitor/message.blade.php
+++ resources/views/visitor/message.blade.php
@@ -8,13 +8,13 @@
       <div class="shape align-items-end justify-content-end" style="margin-right: -8rem;">
         <img loading="lazy" src="{{ asset('/img/speakers/asset-1.svg') }}" alt="" width="420px" height="487">
       </div>
-    <section class="pt-lg-100 pb-100 mt-lg-100 pt-50 mt-50">
+    <section class="pt-lg-100 pb-100 mt-lg-80 pt-50 mt-40">
         <section class="p-5 text-center text-sm-start mb-50">
-           <div class="container">
+           <div class="container">`
              <div class="row"> 
                 <div class="col-lg-8 col-12 text-lg-start text-center align-text-between">
                    <!-- <h1>Become a <span class="text-warning">Web Developer</span></h1> -->
-                   <h1 style="font-family: Marcellus; color: #d5ac44;">Message from the OIC President and Concurrent Vice President for Academic Affairs</h1>
+                   <h1 style="font-family: Marcellus !important; color: #d5ac44;">Message from the<br> OIC President and Concurrent Vice President for Academic Affairs</h1>
                    <!-- <button class="btn btn-primary btn-lg"
                     data-bs-toggle="modal"
                     data-bs-target="#enroll">Start The Enrollment</button> -->
@@ -32,7 +32,7 @@
                <div class="row">
                  <div class="col-12" style="text-align: justify;">
                  <!-- <h1>Become a <span class="text-warning">Web Developer</span></h1> -->
-                 <p class="lead my-4 text-justify" style="font-family: Metropolis; "> 
+                 <p class="my-4 text-justify" style="font-family: Metropolis; "> 
                    We focus on teaching our students the fundamentals of the latest,<br>
                    and greatest technologies to prepare them for their first dev role.
                    We focus on teaching our students the fundamentals of the latest
@@ -89,7 +89,7 @@
                    and greatest technologies to prepare them for their first dev role.
                  </p>
 
-                 <h3 style="font-family: Marcellus;"><br>Professor Ederson DT. Tapia, PhD. DPA</h3>
+                 <h3 style="font-family: Marcellus !important;"><br>Professor Ederson DT. Tapia, PhD. DPA</h3>
                  </div>
               
                </div>
@@ -97,4 +97,9 @@
            </div>
         </section>
     </section>
+    <script>
+      $(function(){
+        $(".navbar").attr("style","background-color: #052964"); 
+      }); 
+    </script>
 @endsection
